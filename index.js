662c10d10fbb86c36de4062ed6c2aa996e8f5aa6
Matt Clements <matt.clements@debitfinance.co.uk>
2016-07-09 05:35:24 +0100

    Added Support for BankAccount Validate

--- index.js
+++ index.js
@@ -104,4 +104,35 @@ exports.RetrieveByAddress= function(data, next) {
 			    }
 			}
 		 });
-};  
+};
+
+exports.BankAccountValidation= function(data, next) {
+
+        requestify.request(baseURL + "/BankAccountValidation/Interactive/Validate/v2.00/json3.ws", {
+            method: "POST",
+			params: {
+				Key: KEY,
+				AccountNumber: data.accountNumber,
+				SortCode: data.sortCode
+		},
+                dataType: "json"
+                })
+                .then(function(response) {
+			var body = JSON.parse(response.body);
+			// Test for an error
+			if (body.Items.length == 1 && typeof(body.Items[0].Error) != "undefined") {
+			    // Show the error message
+			    next( {
+				description: body.Items[0].Description
+			    }, null);
+			}
+			else {
+			    // Check if there were any items found
+			    if (body.Items.length == 0)
+				next(null, []);
+			    else {
+				next(null, body.Items);
+			    }
+			}
+		 });
+};
